69fe1060fd9264703c9892062c1e75e0954b90b1
Saif Ali Khan <saffellikhan@github.com>
2020-05-17 05:03:50 +0500

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
-# epic-link-crawler
+# Epic Link Crawler
+
 A simple in depth links crawler. You can easily collect all the links available on a website.
+
+#### Installation
+
+`$ npm i epic-link-crawler --save`
+
+## Usage
+
+```
+//Crawl all the links from google.com homepage.
+new epicLinkCrawler("https://google.com")
+    .config({
+        depth: 1,
+        strict: true, //Set strict false if you also want to collect links related to other websites.
+    })
+    .crawl() //Triggers Crawling
+    .then((data: string[]) => {
+        console.log(data); //Results Here.
+    }).catch((error: any) => {
+        console.log(error); //Error Here.
+    });
+/**
+ * Expected Resuts On Depth 1
+ *
+ * [
+    'https://play.google.com/?hl=en&tab=w8',
+    'https://mail.google.com/mail/?tab=wm',
+    'https://drive.google.com/?tab=wo',
+    'https://www.google.com/calendar?tab=wc',
+    'https://photos.google.com/?tab=wq&pageId=none',
+    'https://docs.google.com/document/?usp=docs_alc',
+    'https://accounts.google.com/ServiceLogin?hl=en&passive=true&continue=https://www.google.com/',
+    'https://www.google.com/setprefs?sig=0_QLWlMq1910erDBng9UqCXn8pCmQ%3D&hl=ur&source=homepage&sa=X&ved=0ahUKEwiqg6bSwrnpAhUHrxoKHV8bCgQQ2ZgBCAU',
+    'https://www.google.com/setprefs?sig=0_QLWlMq1910erDBng9UqCXn8pCmQ%3D&hl=ps&source=homepage&sa=X&ved=0ahUKEwiqg6bSwrnpAhUHrxoKHV8bCgQQ2ZgBCAY',
+    'https://www.google.com/setprefs?sig=0_QLWlMq1910erDBng9UqCXn8pCmQ%3D&hl=sd&source=homepage&sa=X&ved=0ahUKEwiqg6bSwrnpAhUHrxoKHV8bCgQQ2ZgBCAc',
+    'https://www.google.com/setprefdomain?prefdom=PK&prev=https://www.google.com.pk/&sig=K_BE-rlArupsHUl4I9PADVcxBLCNg%3D',
+    'https://google.com/preferences?hl=en',
+    'https://google.com/advanced_search?hl=en-PK&authuser=0',
+    'https://google.com/intl/en/ads',
+    'https://google.com/intl/en/about.html',
+    'https://google.com/intl/en/policies/privacy',
+    'https://google.com/intl/en/policies/terms'
+    ]
+ */
+```
+
+## Options
+
+Just two options are supported for now.
+
+- _depth_ - \_1 to 5\_ (Default 1) | Crawling Depth.
+- _strict_ - _boolean_ (Default True) | Set to False if you also want to collect links related to other websites.
---
+++ src/epicLinkCrawler.js
@@ -0,0 +1,229 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const events_1 = __importDefault(require("events"));
+const request_1 = __importDefault(require("request"));
+const cheerio_1 = __importDefault(require("cheerio"));
+const url_parse_1 = __importDefault(require("url-parse"));
+// @ts-ignore | Because Not Type Declaration Available!
+const extract_domain_1 = __importDefault(require("extract-domain"));
+//The Class
+class epicLinkCrawler {
+    constructor(url) {
+        this.url = "";
+        this.domain = "";
+        this.urlBase = "";
+        this.options = {
+            depth: 1,
+            strict: true,
+        };
+        this.config = ({ depth = 1, strict = true } = {}) => {
+            let self = this;
+            self.options.depth = depth;
+            self.options.strict = strict;
+            return self;
+        };
+        this.getContent = (url) => {
+            return new Promise((resolve, reject) => {
+                request_1.default(url, (error, response, content) => {
+                    resolve(content);
+                });
+            });
+        };
+        this.collectLinks = (content) => {
+            let self = this;
+            if (typeof content == "undefined")
+                content = "";
+            let $ = cheerio_1.default.load(content);
+            let relativeLinks = $("a[href^='/']").not("[rel='nofollow']");
+            let absoluteLinks = $("a[href^='http']").not("[rel='nofollow']");
+            let relativeLinksArray = [];
+            let absoluteLinksArray = [];
+            //Collect Relative Links
+            relativeLinks.each(function () {
+                let href = $(this).attr("href");
+                if (typeof href != "undefined")
+                    if (relativeLinksArray.indexOf(href) == -1)
+                        relativeLinksArray.push(self.urlBase + "/" + href.replace(/^\/+|\/+$/g, ""));
+            });
+            //Collect Absolute Links
+            absoluteLinks.each(function () {
+                let href = $(this).attr("href");
+                if (typeof href != "undefined") {
+                    if (self.options.strict && self.domain != extract_domain_1.default(href))
+                        return true;
+                    if (absoluteLinksArray.indexOf(href) == -1)
+                        absoluteLinksArray.push(href);
+                }
+            });
+            return Array.from(new Set(absoluteLinksArray.concat(relativeLinksArray)));
+        };
+        this.level1Crawl = (url = "", depth = 1) => {
+            let self = this;
+            return new Promise((resolve, reject) => {
+                let subject = self.url;
+                if (url != "")
+                    subject = url;
+                self.getContent(subject).then(content => {
+                    let crawledLinks = self.collectLinks(content);
+                    self.events.emit("level1Crawl.success", crawledLinks);
+                    resolve(crawledLinks);
+                }).catch(error => {
+                    self.events.emit("level1Crawl.error", error);
+                    reject(error);
+                });
+            });
+        };
+        this.level2Crawl = (url = "") => {
+            let self = this;
+            return new Promise((resolve, reject) => {
+                let subject = self.url;
+                if (url != "")
+                    subject = url;
+                self.level1Crawl(subject).then((crawledLinks) => {
+                    let oldcrawledLinks = crawledLinks;
+                    let childCrawledLinks = [];
+                    crawledLinks.forEach((v, i) => {
+                        self.level1Crawl(v).then((ccl) => {
+                            childCrawledLinks.push(ccl);
+                            if (crawledLinks.length == childCrawledLinks.length) {
+                                let crawledLinks = [];
+                                childCrawledLinks.forEach((v, i) => {
+                                    crawledLinks = crawledLinks.concat(v);
+                                });
+                                self.events.emit("level2Crawl.success", Array.from(new Set(crawledLinks.concat(oldcrawledLinks))));
+                                resolve(Array.from(new Set(crawledLinks.concat(oldcrawledLinks))));
+                            }
+                        }).catch(error => {
+                            self.events.emit("level2Crawl.error", error);
+                            reject(error);
+                        });
+                    });
+                }).catch(error => {
+                    self.events.emit("level2Crawl.error", error);
+                    reject(error);
+                });
+            });
+        };
+        this.level3Crawl = (url = "") => {
+            let self = this;
+            return new Promise((resolve, reject) => {
+                let subject = self.url;
+                if (url != "")
+                    subject = url;
+                self.level2Crawl(subject).then((crawledLinks) => {
+                    let oldcrawledLinks = crawledLinks;
+                    let childCrawledLinks = [];
+                    crawledLinks.forEach((v, i) => {
+                        self.level1Crawl(v).then((ccl) => {
+                            childCrawledLinks.push(ccl);
+                            if (crawledLinks.length == childCrawledLinks.length) {
+                                let crawledLinks = [];
+                                childCrawledLinks.forEach((v, i) => {
+                                    crawledLinks = crawledLinks.concat(v);
+                                });
+                                self.events.emit("level3Crawl.success", Array.from(new Set(crawledLinks.concat(oldcrawledLinks))));
+                                resolve(Array.from(new Set(crawledLinks.concat(oldcrawledLinks))));
+                            }
+                        }).catch(error => {
+                            self.events.emit("level3Crawl.error", error);
+                            reject(error);
+                        });
+                    });
+                }).catch(error => {
+                    self.events.emit("level3Crawl.error", error);
+                    reject(error);
+                });
+            });
+        };
+        this.level4Crawl = (url = "") => {
+            let self = this;
+            return new Promise((resolve, reject) => {
+                let subject = self.url;
+                if (url != "")
+                    subject = url;
+                self.level3Crawl(subject).then((crawledLinks) => {
+                    let oldcrawledLinks = crawledLinks;
+                    let childCrawledLinks = [];
+                    crawledLinks.forEach((v, i) => {
+                        self.level1Crawl(v).then((ccl) => {
+                            childCrawledLinks.push(ccl);
+                            if (crawledLinks.length == childCrawledLinks.length) {
+                                let crawledLinks = [];
+                                childCrawledLinks.forEach((v, i) => {
+                                    crawledLinks = crawledLinks.concat(v);
+                                });
+                                self.events.emit("level4Crawl.success", Array.from(new Set(crawledLinks.concat(oldcrawledLinks))));
+                                resolve(Array.from(new Set(crawledLinks.concat(oldcrawledLinks))));
+                            }
+                        }).catch(error => {
+                            self.events.emit("level4Crawl.error", error);
+                            reject(error);
+                        });
+                    });
+                }).catch(error => {
+                    self.events.emit("level4Crawl.error", error);
+                    reject(error);
+                });
+            });
+        };
+        this.level5Crawl = (url = "") => {
+            let self = this;
+            return new Promise((resolve, reject) => {
+                let subject = self.url;
+                if (url != "")
+                    subject = url;
+                self.level4Crawl(subject).then((crawledLinks) => {
+                    let oldcrawledLinks = crawledLinks;
+                    let childCrawledLinks = [];
+                    crawledLinks.forEach((v, i) => {
+                        self.level1Crawl(v).then((ccl) => {
+                            childCrawledLinks.push(ccl);
+                            if (crawledLinks.length == childCrawledLinks.length) {
+                                let crawledLinks = [];
+                                childCrawledLinks.forEach((v, i) => {
+                                    crawledLinks = crawledLinks.concat(v);
+                                });
+                                self.events.emit("level5Crawl.success", Array.from(new Set(crawledLinks.concat(oldcrawledLinks))));
+                                resolve(Array.from(new Set(crawledLinks.concat(oldcrawledLinks))));
+                            }
+                        }).catch(error => {
+                            self.events.emit("level5Crawl.error", error);
+                            reject(error);
+                        });
+                    });
+                }).catch(error => {
+                    self.events.emit("level5Crawl.error", error);
+                    reject(error);
+                });
+            });
+        };
+        this.crawl = (url = "") => {
+            let self = this;
+            return self["level" + self.options.depth + "Crawl"](url);
+        };
+        this.on = (event, handler) => {
+            let self = this;
+            this.events.on(event, handler);
+            return self;
+        };
+        //Resolve Required Modules
+        require.resolve("events");
+        require.resolve("request");
+        require.resolve("cheerio");
+        require.resolve("url-parse");
+        //Assignment
+        this.events = new events_1.default.EventEmitter();
+        this.url = url.replace(/^\/+|\/+$/g, "");
+        this.domain = extract_domain_1.default(this.url);
+        this.urlObject = new url_parse_1.default(this.url);
+        if (this.urlObject.origin == "null")
+            this.events.emit("crawl.error", "Invalid URL Has Been Provided!");
+        this.urlBase = this.urlObject.protocol + "//" + this.urlObject.hostname;
+        return this;
+    }
+}
+//Export Module
+module.exports.epicLinkCrawler = epicLinkCrawler;
